da637f182e649cb1da3166c6617118a70c373285
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-05-27 00:07:43 +0530

    labelling working....need to optimize code...alot

--- app/src/main/java/jv/rm/iamjosephvarghese/ml_kit_image_labelling/MainActivity.java
+++ app/src/main/java/jv/rm/iamjosephvarghese/ml_kit_image_labelling/MainActivity.java
@@ -1,13 +1,228 @@
 package jv.rm.iamjosephvarghese.ml_kit_image_labelling;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions;
+
+import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Uri uriPhoto;
+    Uri uploadUri;
+    private Bitmap bitmap;
+
+    private int CAMERA_PERMISSION_CODE = 24;
+
+    private int TAKE_CAMERA = 2;
+
+    Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+
+
+        StrictMode.VmPolicy.Builder builderStrict = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builderStrict.build());
+
+
+
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (isCameraAllowed()){
+                        openCamera();
+                        return;
+                    }
+                    requestCamera();
+                }else {
+                    openCamera();
+                }
+            }
+
+        });
+
+
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(requestCode == TAKE_CAMERA && resultCode == RESULT_OK) {
+            Uri selectedImage = uriPhoto;
+            uploadUri = uriPhoto;
+
+
+
+
+            try {
+                this.getContentResolver().notifyChange(selectedImage, null);
+                ContentResolver cr = getContentResolver();
+                bitmap = android.provider.MediaStore.Images.Media
+                        .getBitmap(cr, selectedImage);
+
+                Log.d("bitmap",bitmap.toString());
+                //img.setImageBitmap(bitmap);
+                //not showing image as already shown in camera view
+
+                labelImage(bitmap);
+
+            } catch (Exception e) {
+                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                Log.e("Camera", e.toString());
+            }
+
+
+//            labelImage(bitmap);
+
+
+        }
+
+
+    }
+
+    private void openCamera() {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),  "Pic.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        uriPhoto = Uri.fromFile(photo);
+        startActivityForResult(intent, TAKE_CAMERA);
+    }
+
+
+
+    //We are calling this method to check the permission status
+    private boolean isCameraAllowed() {
+        //Getting the permission status
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
+
+        //If permission is granted returning true
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        //If permission is not granted returning false
+        return false;
+    }
+
+
+
+    //Requesting permission
+    private void requestCamera(){
+
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAMERA)){
+//            //If the user has denied the permission previously your code will come to this block
+//            //Here you can explain why you need this permission
+//            //Explain here why you need this permission
+//        }
+
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.CAMERA},CAMERA_PERMISSION_CODE);
+    }
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == CAMERA_PERMISSION_CODE){
+
+            //If permission is granted
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                //Displaying a toast
+                Toast.makeText(this,"Permission granted.Click Selfie Again.",Toast.LENGTH_LONG).show();
+            }else{
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this,"Oops you just denied the permission",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    void labelImage(Bitmap bitmap){
+
+//        FirebaseVisionLabelDetectorOptions options =
+//                new FirebaseVisionLabelDetectorOptions.Builder()
+//                        .setConfidenceThreshold(0.8f)
+//                        .build();
+
+        FirebaseApp.initializeApp(this);
+
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
+                .getVisionLabelDetector();
+
+        final Task<List<FirebaseVisionLabel>> result =
+                detector.detectInImage(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<FirebaseVisionLabel>>() {
+                                    @Override
+                                    public void onSuccess(List<FirebaseVisionLabel> labels) {
+
+                                        for (FirebaseVisionLabel label: labels) {
+                                            String text = label.getLabel();
+                                            String entityId = label.getEntityId();
+                                            float confidence = label.getConfidence();
+                                            Log.d("results","-"+text +"-"+ entityId);
+                                        }
+
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+
+    }
+
+
+
 }

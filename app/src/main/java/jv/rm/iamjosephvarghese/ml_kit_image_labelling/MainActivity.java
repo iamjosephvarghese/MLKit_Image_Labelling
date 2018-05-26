b3de033566d5e7037cec3394b708b42c6382034c
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-05-26 09:22:48 +0530

    Initial Commit with dependencies for mlkit

---
+++ app/src/main/java/jv/rm/iamjosephvarghese/ml_kit_image_labelling/MainActivity.java
@@ -0,0 +1,13 @@
+package jv.rm.iamjosephvarghese.ml_kit_image_labelling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
